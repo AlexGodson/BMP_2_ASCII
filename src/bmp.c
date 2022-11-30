b7e13cf601fbd3f0409258fae58c01971200ffdc
AlexGodson <alexgodson999@gmail.com>
2022-11-30 02:08:24 +0000

    more stuff done, but it dont work soo

--- src/bmp.c
+++ src/bmp.c
@@ -4,8 +4,24 @@
 #include "../Include/bmp.h"
 
 // Get some head bitches
-void get_BMP_HEAD(FILE *fptr, struct BMP_HEADER *image_file); {
-    
+void get_BMP_head(char *filepath, struct BMP_HEADER *bmp_head) {
+    FILE *fptr = fopen(filepath, "rb");
+
+    if (fptr == NULL) {
+        printf("Error opening file: %s\n", filepath);
+        exit(-1);
+    }
+
+    printf("size of the stuct is: %ld", sizeof(*bmp_head));
+
+    fread(bmp_head, sizeof(*bmp_head), 1, fptr);
+
+    if (bmp_head->type != 0x4D42) {
+        printf("ERROR: Invalid file, not a BMP\n");
+        exit(-1);
+    }
+
+    fclose(fptr);
 }
 
 
--- src/main.c
+++ src/main.c
@@ -10,20 +10,25 @@ int main(int argc, char **argv) {
         exit(-1);
     }
 
-    FILE *fptr = fopen(argv[1], "r");
+    struct BMP_HEADER image_head;
+    get_BMP_head(*(argv + 1), &image_head);
 
-    if (fptr == NULL) {
-        printf("Error opening file: %s\n", argv[1]);
-        exit(-1);
-    }
+    printf("Image: type: %d\n", image_head.type);
+    printf("Image: size: %d\n", image_head.size);
+    printf("Image: reserved1: %d\n", image_head.reserved1);
+    printf("Image: reserved2: %d\n", image_head.reserved2);
+    printf("Image: offset: %d\n", image_head.offset);
+    printf("Image: dib_header_size: %d\n", image_head.dib_header_size);
+    printf("Image: width_px: %d\n", image_head.width_px);
+    printf("Image: height_px: %d\n", image_head.height_px);
+    printf("Image: num_planes: %d\n", image_head.num_planes);
+    printf("Image: bits_per_pixel: %d\n", image_head.bits_per_pixel);
+    printf("Image: compression: %d\n", image_head.compression);
+    printf("Image: image_size_bytes: %d\n", image_head.image_size_bytes);
+    printf("Image: x_resolution_ppm: %d\n", image_head.x_resolution_ppm);
+    printf("Image: y_resolution_ppm: %d\n", image_head.y_resolution_ppm);
+    printf("Image: num_colors: %d\n", image_head.num_colors);
+    printf("Image: important_colors: %d\n", image_head.important_colors);
 
-    struct BMP_head image_head;
-    get_format(fptr, &image_head);
-
-    printf("DATA TYPE: %c, %c\n\n", image_head.type[0], image_head.type[1]);
-
-
-    // Closing the BMP file
-    fclose(fptr);
     return 0;
 }
